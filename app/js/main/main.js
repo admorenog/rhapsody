eaecc8a4dbfe7c7543f58578cbbd12dc3cea496b
Adrián Moreno García <adrianmorenogarcia@gmail.com>
2019-03-31 08:52:22 +0200

    Añadidos ficheros de configuración de ts, scss y gestor de plantillas ejs

---
+++ app/js/main/main.js
@@ -0,0 +1,57 @@
+const { app, BrowserWindow } = require('electron')
+const ElectronViewRenderer = require('electron-view-renderer')
+const fs = require('fs')
+const path = require('path')
+
+let config = JSON.parse(fs.readFileSync(app.getAppPath() + path.sep + 'config.json'));
+
+let ctx = { config: config, render: null, mainWindow: null };
+
+ctx.render = new ElectronViewRenderer({
+	viewPath: 'app/views',
+	viewProtcolName: 'view',
+	useAssets: true,
+	assetsPath: 'assets',
+	assetsProtocolName: 'asset'
+})
+
+ctx.render.use('ejs')
+
+function createWindow() {
+	// Create the browser window.
+	ctx.mainWindow = new BrowserWindow({ width: 800, height: 600 });
+	// mainWindow.loadFile('main.html');
+
+	ctx.render.load(ctx.mainWindow, 'main', { ctx: ctx })
+
+	if (config.debug) {
+		ctx.mainWindow.webContents.openDevTools()
+	}
+
+	// Emitted when the window is closed.
+	ctx.mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	});
+}
+
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
+	// On macOS it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin' || !config.debug) {
+		app.quit()
+	}
+});
+app.on('activate', function () {
+	// On macOS it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow()
+	}
+});
+
+require('./js/main/menu')
+require('./js/main/tray')
---
+++ app/js/main/tray.js
@@ -0,0 +1,22 @@
+const {
+	app,
+	Tray,
+	Menu,
+	nativeImage
+} = require('electron');
+
+let tray = null
+app.on( 'ready', () => {
+	let iconPath = './resources/images/icon.png';
+	let trayIcon = nativeImage.createFromPath( iconPath );
+	trayIcon = trayIcon.resize( { width: 16, height: 16 } );
+	tray = new Tray( trayIcon );
+	const contextMenu = Menu.buildFromTemplate( [
+		{ label: 'Item1', type: 'radio' },
+		{ label: 'Item2', type: 'radio' },
+		{ label: 'Item3', type: 'radio', checked: true },
+		{ label: 'Item4', type: 'radio' }
+	] );
+	tray.setToolTip( 'Flex query builder beta' );
+	tray.setContextMenu( contextMenu )
+})
