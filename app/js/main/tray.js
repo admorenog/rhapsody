db7183636f68e8900f3d4530e5b81fd3efd31a33
Adrián Moreno García <adrianmorenogarcia@gmail.com>
2019-03-31 09:29:07 +0200

    Configuradas las tareas de compilación de ts

--- app/js/main/tray.js
+++ app/js/main/tray.js
@@ -1,22 +1,17 @@
-const {
-	app,
-	Tray,
-	Menu,
-	nativeImage
-} = require('electron');
-
-let tray = null
-app.on( 'ready', () => {
-	let iconPath = './resources/images/icon.png';
-	let trayIcon = nativeImage.createFromPath( iconPath );
-	trayIcon = trayIcon.resize( { width: 16, height: 16 } );
-	tray = new Tray( trayIcon );
-	const contextMenu = Menu.buildFromTemplate( [
-		{ label: 'Item1', type: 'radio' },
-		{ label: 'Item2', type: 'radio' },
-		{ label: 'Item3', type: 'radio', checked: true },
-		{ label: 'Item4', type: 'radio' }
-	] );
-	tray.setToolTip( 'Flex query builder beta' );
-	tray.setContextMenu( contextMenu )
-})
+const { Tray, nativeImage } = require('electron');
+let tray = null;
+app.on('ready', () => {
+    let iconPath = './resources/images/icon.png';
+    let trayIcon = nativeImage.createFromPath(iconPath);
+    trayIcon = trayIcon.resize({ width: 16, height: 16 });
+    tray = new Tray(trayIcon);
+    const contextMenu = Menu.buildFromTemplate([
+        { label: 'Item1', type: 'radio' },
+        { label: 'Item2', type: 'radio' },
+        { label: 'Item3', type: 'radio', checked: true },
+        { label: 'Item4', type: 'radio' }
+    ]);
+    tray.setToolTip('Flex query builder beta');
+    tray.setContextMenu(contextMenu);
+});
+//# sourceMappingURL=tray.js.map
